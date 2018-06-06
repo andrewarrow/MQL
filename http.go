1f072241231b804f26ede7286bdeac9ed8014713
Andrew Arrow <andrew@bird.co>
2018-06-06 20:24:47 +0000

    p

--- http.go
+++ http.go
@@ -3,7 +3,48 @@ package main
 import "fmt"
 import "io/ioutil"
 import "net/http"
+import "bytes"
+import "encoding/json"
 import "encoding/base64"
+
+func DoPVerb(verb string, route string, params map[string]interface{}) string {
+
+	var buf, _ = json.Marshal(params)
+	body := bytes.NewBuffer(buf)
+
+	m := conf()
+	token := m["token"]
+	secret := m["secret"]
+	prefix := m["url"]
+	name := m["name"]
+	url := fmt.Sprintf("%s/%s/%s", prefix, name, route)
+	request, _ := http.NewRequest(verb, url, body)
+
+	sEnc := base64.StdEncoding.EncodeToString([]byte(token + ":" + secret))
+
+	request.Header.Set("Authorization", "BASIC "+sEnc)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/hal+json")
+	client := &http.Client{}
+
+	resp, err := client.Do(request)
+	if err == nil {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			if resp.StatusCode == 200 {
+				return string(body)
+			} else {
+				fmt.Println(string(body))
+			}
+		} else {
+			fmt.Println(string(body), err)
+		}
+	} else {
+		fmt.Println(err)
+	}
+	return ""
+}
 
 func DoVerb(route string) string {
 	m := conf()
